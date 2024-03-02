d4d7551d234d8dabc459ce34ea887fa842f1ebec
ayaanqui <ayaanqui.com@gmail.com>
2024-03-02 17:03:01 -0600

    feat: add transaction prop to service struct

--- services/service.go
+++ services/service.go
@@ -3,12 +3,23 @@ package services
 import (
 	"database/sql"
 
+	"github.com/go-jet/jet/v2/qrm"
 	"github.com/go-playground/validator/v10"
 	"github.com/stadio-app/stadio-backend/types"
 )
 
 type Service struct {
 	DB *sql.DB
+	TX *sql.Tx
 	StructValidator *validator.Validate
 	Tokens types.Tokens
+}
+
+// Returns a transaction if present.
+// Otherwise returns the Database connection instance as qrm.Queryable
+func (service *Service) DbOrTxQueryable() qrm.Queryable {
+	if service.TX != nil {
+		return service.TX
+	}
+	return service.DB
 }
