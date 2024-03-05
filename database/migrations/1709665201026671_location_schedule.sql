dad0a93adbab203b9a140cf00fce9d070a283e09
ayaanqui <ayaanqui.com@gmail.com>
2024-03-05 14:20:14 -0600

    feat: create `location_schedule` table

---
+++ database/migrations/1709665201026671_location_schedule.sql
@@ -0,0 +1,20 @@
+CREATE TYPE "week_day" AS ENUM (
+  'SUNDAY',
+  'MONDAY',
+  'TUESDAY',
+  'WEDNESDAY',
+  'THURSDAY',
+  'FRIDAY',
+  'SATURDAY'
+);
+
+CREATE TABLE "location_schedule" (
+  "id" BIGSERIAL UNIQUE PRIMARY KEY,
+  "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(), 
+  "updated_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(), 
+  "location_id" BIGINT REFERENCES "location"("id") ON DELETE CASCADE, 
+  "day" "week_day" NOT NULL,
+  "from" INTEGER DEFAULT 9, -- 24 hour time - 9 am
+  "to" INTEGER DEFAULT 17, -- 24 hour time - 5 pm
+  "available" BOOLEAN DEFAULT FALSE
+);
