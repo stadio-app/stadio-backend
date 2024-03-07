83791621e64fc8176f5bf881ff601978c599b09c
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2024-03-06 23:02:53 -0600

    Location Schedule (#26)

--- database/jet/postgres/public/table/table_use_schema.go
+++ database/jet/postgres/public/table/table_use_schema.go
@@ -15,6 +15,7 @@ func UseSchema(schema string) {
 	EmailVerification = EmailVerification.FromSchema(schema)
 	Event = Event.FromSchema(schema)
 	Location = Location.FromSchema(schema)
+	LocationSchedule = LocationSchedule.FromSchema(schema)
 	Migration = Migration.FromSchema(schema)
 	Owner = Owner.FromSchema(schema)
 	Participant = Participant.FromSchema(schema)
