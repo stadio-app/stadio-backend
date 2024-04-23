7ef8b8f5d09b9e626419f228c866d3f086b0cb64
ayaanqui <ayaanqui.com@gmail.com>
2024-04-23 11:40:30 -0500

    fix: location_id not null constraint

--- database/migrations/1713882521714538_location_image.sql
+++ database/migrations/1713882521714538_location_image.sql
@@ -4,7 +4,7 @@ CREATE TABLE "location_image" (
     "updated_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     "upload_id" TEXT NOT NULL UNIQUE,
     "original_filename" VARCHAR(255) NOT NULL,
-    "location_id" BIGINT REFERENCES "location"("id") ON DELETE CASCADE,
+    "location_id" BIGINT REFERENCES "location"("id") ON DELETE CASCADE NOT NULL,
     "default" BOOLEAN NOT NULL DEFAULT FALSE,
     "caption" TEXT,
     "created_by" BIGINT REFERENCES "user"("id") ON DELETE SET NULL, 
