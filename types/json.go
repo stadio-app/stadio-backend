f27b4a922a30732ef52b8cae3510aa506fc854b1
ayaanqui <ayaanqui.com@gmail.com>
2024-04-23 11:41:51 -0500

    feat: cf token types

--- types/json.go
+++ types/json.go
@@ -5,15 +5,21 @@ type SendGridTemplates struct {
 }
 
 type SendGridTokens struct {
-	ApiKey string `json:"api_key"`
-	RecoveryCode string `json:"recovery_code,omitempty"`
-	ApiKeyId string `json:"api_key_id,omitempty"`
-	Templates SendGridTemplates `json:"templates,omitempty"`
+	ApiKey       string            `json:"apiKey"`
+	RecoveryCode string            `json:"recoveryCode,omitempty"`
+	ApiKeyId     string            `json:"apiKeyId,omitempty"`
+	Templates    SendGridTemplates `json:"templates,omitempty"`
+}
+
+type CloudflareTokens struct {
+	ApiKey   string `json:"apiKey"`
+	ApiEmail string `json:"apiEmail"`
 }
 
 type Tokens struct {
-	JwtKey string `json:"jwtKey"`
-	SendGrid SendGridTokens `json:"sendGrid,omitempty"`
+	JwtKey     string           `json:"jwtKey"`
+	SendGrid   SendGridTokens   `json:"sendGrid"`
+	Cloudflare CloudflareTokens `json:"cloudflare"`
 	// To add other tokens create a struct and add them here,
 	// make sure to also update ./tokens.json
 }
