73a942e541b20e02bea6efe589bf3fc89a8589c2
ayaanqui <ayaanqui.com@gmail.com>
2024-04-27 22:58:44 -0500

    feat: cloudinary upload helper method

--- services/image_upload_service.go
+++ services/image_upload_service.go
@@ -4,20 +4,13 @@ import (
 	"context"
 
 	"github.com/99designs/gqlgen/graphql"
-	"github.com/cloudflare/cloudflare-go"
+	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
 )
 
-func (service Service) CloudflareImageUpload(ctx context.Context, data cloudflare.UploadImageParams) (cloudflare.Image, error) {
-	cf_resource_container := cloudflare.ResourceContainer{
-		Level: cloudflare.UserRouteLevel,
-		Type: cloudflare.UserType,
-		Identifier: "<PASTE_IDENTIFIER_HERE>",
-	}
-	return service.CloudflareApi.UploadImage(ctx, &cf_resource_container, data)
-}
-
-func (service Service) GraphImageUpload(ctx context.Context, file graphql.Upload) (cloudflare.Image, error) {
-	return service.CloudflareImageUpload(ctx, cloudflare.UploadImageParams{
-		File: nil,
-	})
+func (service Service) GraphImageUpload(
+	ctx context.Context,
+	file graphql.Upload,
+	params uploader.UploadParams,
+) (*uploader.UploadResult, error) {
+	return service.Cloudinary.Upload.Upload(ctx, file.File, params)
 }
